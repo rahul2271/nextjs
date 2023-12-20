ccc739ef07cca84daeee714c951fd98fe1bb77d7
makecodefun22 <makecodefun22@gmail.com>
2023-12-20 12:07:39 +0530

    D

--- next.config.js
+++ next.config.js
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
-
-module.exports = {
-    typescript: {
-      // !! WARN !!
-      // Dangerously allow production builds to successfully complete even if
-      // your project has type errors.
-      // !! WARN !!
-      ignoreBuildErrors: true,
-    },
-  }
+module.exports = nextConfig;
